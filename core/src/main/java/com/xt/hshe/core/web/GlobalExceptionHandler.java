c59f2738b635f70ad05a00931383de574465c1c4
shawnsky <5835007@qq.com>
2018-03-28 20:21:13 +0800

    后台页面无权自动跳转

--- core/src/main/java/com/xt/hshe/core/web/GlobalExceptionHandler.java
+++ core/src/main/java/com/xt/hshe/core/web/GlobalExceptionHandler.java
@@ -1,13 +1,10 @@
 package com.xt.hshe.core.web;
 
-import com.sun.deploy.net.HttpResponse;
 import com.xt.hshe.core.pojo.HttpMsg;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 /**
  * 全局异常处理器
